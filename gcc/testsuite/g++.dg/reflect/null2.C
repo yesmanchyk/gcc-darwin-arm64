3878972b1abbc3f1704661906bd9861de659ab0c
Marek Polacek <polacek@redhat.com>
2025-06-27 15:34:33 -0400

    meta::info should be const-default-constructible

--- gcc/testsuite/g++.dg/reflect/null2.C
+++ gcc/testsuite/g++.dg/reflect/null2.C
@@ -3,6 +3,5 @@
 // Here, x is a null reflection so should be allowed to compare equal
 // to itself.  meta::info should be const-default-constructible.
 
-// TODO FIX ICE
-//constexpr decltype(^^::) x;
-//static_assert(x == x);
+constexpr decltype(^^::) x;
+static_assert(x == x);
