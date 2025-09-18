278afa47e64be48002c886775f083fa57305650e
Jakub Jelinek <jakub@redhat.com>
2025-09-18 08:58:03 +0200

    __is_reflection trait
    Note, the is_reflection/ tests ICE with -g when trying to emit debug info
    for the templates, that is something that should be handled later.

---
+++ libstdc++-v3/testsuite/20_util/is_reflection/value.cc
@@ -0,0 +1,37 @@
+// { dg-do compile { target c++26 } }
+// { dg-additional-options "-freflection" }
+
+// Copyright (C) 2025 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include <type_traits>
+#include <testsuite_tr1.h>
+
+void test01()
+{
+  using std::is_reflection;
+  using namespace __gnu_test;
+  int v = 1;
+
+  static_assert(test_category<is_reflection, decltype(^^long)>(true), "");
+  static_assert(test_category<is_reflection, const decltype(^^test01)>(true), "");
+  static_assert(test_category<is_reflection, volatile decltype(^^__gnu_test)>(true), "");
+  static_assert(test_category<is_reflection, const volatile decltype(^^v)>(true), "");
+
+  // Sanity check.
+  static_assert(test_category<is_reflection, int>(false), "");
+}
