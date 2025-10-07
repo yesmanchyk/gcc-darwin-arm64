018faafb16f7e4fb3a9f9bb44ac43360c412e794
Jakub Jelinek <jakub@redhat.com>
2025-10-07 11:35:34 +0200

    Use same function for both make_signed and make_unsigned, they are very similar.

--- gcc/cp/reflect.cc
+++ gcc/cp/reflect.cc
@@ -2247,14 +2247,14 @@ eval_add_rvalue_reference (location_t loc, const constexpr_ctx *ctx, tree type,
   return get_reflection_raw (loc, type);
 }
 
-/* Process std::meta::make_signed.
+/* Process std::meta::make_signed and std::meta::make_unsigned.
    Returns: a reflection representing the type denoted by
-   std::make_signed_t<T>, where T is the type or type alias
-   represented by type.  */
+   std::make_signed_t<T> or std::make_unsigned_t<T>, respectively, where T is
+   the type or type alias represented by type.  */
 
 static tree
 eval_make_signed (location_t loc, const constexpr_ctx *ctx, tree type,
-		  tree *jump_target)
+		  bool unsignedp, tree *jump_target)
 {
   if (eval_is_type (type) != boolean_true_node)
     return throw_exception_nontype (loc, ctx, type, jump_target);
@@ -2280,68 +2280,16 @@ eval_make_signed (location_t loc, const constexpr_ctx *ctx, tree type,
       for (unsigned i = 0; i < ARRAY_SIZE (types); ++i)
 	if (tree_int_cst_equal (TYPE_SIZE_UNIT (types[i]), unit))
 	  {
-	    ret = types[i];
+	    ret = c_common_signed_or_unsigned_type (unsignedp, types[i]);
 	    break;
 	  }
       if (!ret)
-	ret = c_common_type_for_size (TYPE_PRECISION (type), 0);
+	ret = c_common_type_for_size (TYPE_PRECISION (type), unsignedp);
     }
   else if (TYPE_MAIN_VARIANT (type) == char_type_node)
-    ret = signed_char_type_node;
-  else if (TYPE_UNSIGNED (type))
-    ret = c_common_signed_type (type);
-  if (ret != type)
-    {
-      int quals = cp_type_quals (type);
-      quals &= (TYPE_QUAL_CONST | TYPE_QUAL_VOLATILE);
-      ret = cp_build_qualified_type (ret, quals);
-    }
-  return get_reflection_raw (loc, ret);
-}
-
-/* Process std::meta::make_unsigned.
-   Returns: a reflection representing the type denoted by
-   std::make_unsigned_t<T>, where T is the type or type alias
-   represented by type.  */
-
-static tree
-eval_make_unsigned (location_t loc, const constexpr_ctx *ctx, tree type,
-		    tree *jump_target)
-{
-  if (eval_is_type (type) != boolean_true_node)
-    return throw_exception_nontype (loc, ctx, type, jump_target);
-  // TODO: I don't see the standard specifying what to do here.
-  if (!INTEGRAL_TYPE_P (type) || TREE_CODE (type) == BOOLEAN_TYPE)
-    return throw_exception (loc, ctx, N_("reflection represents non-integral "
-					 "or bool type"), type, jump_target);
-  tree ret = type;
-  if (TREE_CODE (type) == ENUMERAL_TYPE
-      || TYPE_MAIN_VARIANT (type) == wchar_type_node
-      || TYPE_MAIN_VARIANT (type) == char8_type_node
-      || TYPE_MAIN_VARIANT (type) == char16_type_node
-      || TYPE_MAIN_VARIANT (type) == char32_type_node)
-    {
-      tree unit = TYPE_SIZE_UNIT (type);
-      tree types[] = {
-	unsigned_char_type_node,
-	short_unsigned_type_node,
-	unsigned_type_node,
-	long_unsigned_type_node,
-	long_long_unsigned_type_node };
-      ret = NULL_TREE;
-      for (unsigned i = 0; i < ARRAY_SIZE (types); ++i)
-	if (tree_int_cst_equal (TYPE_SIZE_UNIT (types[i]), unit))
-	  {
-	    ret = types[i];
-	    break;
-	  }
-      if (!ret)
-	ret = c_common_type_for_size (TYPE_PRECISION (type), 1);
-    }
-  else if (TYPE_MAIN_VARIANT (type) == char_type_node)
-    ret = unsigned_char_type_node;
-  else if (!TYPE_UNSIGNED (type))
-    ret = c_common_unsigned_type (type);
+    ret = unsignedp ? unsigned_char_type_node : signed_char_type_node;
+  else if (unsignedp ^ (!!TYPE_UNSIGNED (type)))
+    ret = c_common_signed_or_unsigned_type (unsignedp, type);
   if (ret != type)
     {
       int quals = cp_type_quals (type);
@@ -2732,9 +2680,9 @@ process_metafunction (const constexpr_ctx *ctx, tree call,
   if (id_equal (name, "add_rvalue_reference"))
     return eval_add_rvalue_reference (loc, ctx, h, jump_target);
   if (id_equal (name, "make_signed"))
-    return eval_make_signed (loc, ctx, h, jump_target);
+    return eval_make_signed (loc, ctx, h, false, jump_target);
   if (id_equal (name, "make_unsigned"))
-    return eval_make_unsigned (loc, ctx, h, jump_target);
+    return eval_make_signed (loc, ctx, h, true, jump_target);
   if (id_equal (name, "remove_extent"))
     return eval_remove_extent (loc, ctx, h, jump_target);
   if (id_equal (name, "remove_all_extents"))
