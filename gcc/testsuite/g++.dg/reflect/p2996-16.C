5158ef69d1f59a117b79677b9fd4316be950d717
Jakub Jelinek <jakub@redhat.com>
2025-10-22 10:33:22 +0200

    Add assertions to verify the resulting array.

--- gcc/testsuite/g++.dg/reflect/p2996-16.C
+++ gcc/testsuite/g++.dg/reflect/p2996-16.C
@@ -13,3 +13,6 @@ constexpr std::array sizes = []{
   std::ranges::transform(types, r.begin(), std::meta::size_of);
   return r;
 }();
+static_assert (sizes[0] == sizeof (int));
+static_assert (sizes[1] == sizeof (float));
+static_assert (sizes[2] == sizeof (double));
