ef9b3c122a5e46479757a253a619b78dff071a87
Jakub Jelinek <jakub@redhat.com>
2025-11-06 18:25:17 +0100

    2 new testcases which don't work right now (with TODO and commented out parts).

---
+++ gcc/testsuite/g++.dg/reflect/define_aggregate5.C
@@ -0,0 +1,29 @@
+// { dg-do compile { target c++26 } }
+// { dg-additional-options "-freflection" }
+
+#include <meta>
+#include <ranges>
+
+struct foo;
+
+consteval {
+  using namespace std::meta;
+  using namespace std::views;
+  using std::make_pair;
+  using std::vector;
+  using std::pair;
+#if 0
+  // TODO: This doesn't work somewhere deep in <ranges>.  Some lambda
+  // inside of a constexpr function isn't automatically constexpr?
+  define_aggregate (^^foo, join (vector <vector <pair <bool, info>>> {
+    { make_pair (true, data_member_spec (^^int, { .name = "i" })) },
+    { make_pair (false, data_member_spec (^^std::string, { .name = "s" })),
+      make_pair (true, data_member_spec (^^bool, { .name = "b" })) }})
+	| filter ([] (auto x) constexpr { return x.first; })
+	| transform ([] (auto x) constexpr { return x.second; }));
+#endif
+}
+
+//static_assert (type_of (^^foo::i) == ^^int);
+//static_assert (type_of (^^foo::b) == ^^bool);
+//static_assert (nonstatic_data_members_of (^^foo, std::meta::access_context::current ()).size () == 2);
